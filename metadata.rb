2f4b21db8d860a5155e3b265a3d95062ce13ff76
Michael Hedgpeth <mhedgpeth@gmail.com>
2017-04-25 16:25:22 -0500

    Bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mhedgpeth@gmail.com'
 license 'all_rights'
 description 'Installs/Configures cafe'
 long_description 'Installs/Configures cafe'
-version '0.3.1'
+version '0.3.2'
 issues_url 'https://github.com/mhedgpeth/cafe-cookbook/issues'
 source_url 'https://github.com/mhedgpeth/cafe-cookbook'
 
