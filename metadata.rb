47111401a76a7a3b0c44ecb341e310744bd0f000
Michael Hedgpeth <mhedgpeth@gmail.com>
2017-05-04 14:37:06 -0500

    'v1.3.1'

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures cafe'
 long_description 'Installs/Configures cafe'
 issues_url 'https://github.com/mhedgpeth/cafe-cookbook/issues'
 source_url 'https://github.com/mhedgpeth/cafe-cookbook'
-version '1.3.0'
+version '1.3.1'
 
 supports 'windows'
 
